fa9aa5ca0e7c28e00a06268fe3db20d6a40953c6
fiidhah <fidharahman936@gmail.com>
2026-03-23 14:51:41 +0530

    Add month name echo script in pg50.sh

---
+++ pg50.sh
@@ -0,0 +1,21 @@
+while true
+do
+    echo "Enter month number (1-12):"
+    read m
+
+    case $m in
+    1) echo "January" ;;
+    2) echo "February" ;;
+    3) echo "March" ;;
+    4) echo "April" ;;
+    5) echo "May" ;;
+    6) echo "June" ;;
+    7) echo "July" ;;
+    8) echo "August" ;;
+    9) echo "September" ;;
+    10) echo "October" ;;
+    11) echo "November" ;;
+    12) echo "December" ;;
+    *) echo "Invalid" ;;
+    esac
+done
